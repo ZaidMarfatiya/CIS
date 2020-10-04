e2b3022b8858709991534acb557e87a55dbac09c
Zabbir99 <72246539+Zabbir99@users.noreply.github.com>
2020-10-04 09:59:25 +0530

    Added some informative comments

--- CIS/homepage.aspx.vb
+++ CIS/homepage.aspx.vb
@@ -1,6 +1,6 @@
 ﻿Public Class homepage
     Inherits System.Web.UI.Page
-
+    'Loads the system UI
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
@@ -14,14 +14,14 @@
     End Sub
 
     Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        Response.Redirect("pdf/farmer2.pdf")
+        Response.Redirect("pdf/farmer2.pdf") 'Redirect to farmer2 pdf
     End Sub
 
     Protected Sub Button4_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        Response.Redirect("pdf/january_2016.pdf")
+        Response.Redirect("pdf/january_2016.pdf") 'Redirect to january_2016 pdf
     End Sub
 
     Protected Sub Button5_Click(sender As Object, e As EventArgs) Handles Button10.Click
-        Response.Redirect("pdf/farmer4.pdf")
+        Response.Redirect("pdf/farmer4.pdf") 'Redirect to farmer4 pdf
     End Sub
 End Class
